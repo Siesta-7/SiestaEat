66dc55386f0869179d8ed41de4b2777d84b2ccdc
Burhan Avcı <96354957+Siesta-7@users.noreply.github.com>
2023-10-24 20:04:24 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='h-12 p-4 md:h-24 md:px-8 lg:px-20 xl:px-28 flex items-center justify-between text-red-500 uppercase '>
-      <Link className='font-bold text-xl' href="/">Massimo</Link>
+      <Link className='font-bold text-xl' href="/">SiestaEat</Link>
       <p>All Rights Reserved</p>
     </div>
   )
